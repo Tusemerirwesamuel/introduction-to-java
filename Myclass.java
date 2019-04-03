d0fb857734b46190c1366c82067e3ab0e8aa5b79
Tusemerirwesamuel <43774688+Tusemerirwesamuel@users.noreply.github.com>
2019-04-03 13:52:34 +0300

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,31 @@
+// day one in java programming
+public class Myclass {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+// Java Variables
+                // String
+public class MyClass {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+  }
+}
+
+                  //  int
+public class MyClass {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    System.out.println(x + y); // Print the value of x + y
+  }
+}
+                    // Examples
+// int myNum = 5;
+// float myFloatNum = 5.99f;
+// char myLetter = 'D';
+// boolean myBool = true;
+// String myText = "Hello";
+
+
